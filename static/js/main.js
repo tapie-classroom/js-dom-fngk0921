5bef91acaa741a6ebc96a892d89c45328d51d695
fngk0921 <25s102@sunrint.hs.kr>
2025-05-01 21:49:09 +0900

    Add scaling effect and audio feedback on cat image click; improve CPS calculation

--- static/js/main.js
+++ static/js/main.js
@@ -1,38 +1,62 @@
+let currentCps = 0;
+
+const wait = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+
 window.onload = () => {
+    let cpsList = [];
     const clickArea = document.getElementById("app");
     const catImg = document.getElementById("cat-img");
     let cps = 0;
     const popAudio = new Audio("https://popcat.click/pops/pop4.mp3");
     popAudio.load();
 
-    const startTime = new Date().getTime();
+    let startTime = new Date().getTime();
 
-    clickArea.addEventListener("mousedown", function() {
+    clickArea.addEventListener("mousedown", async function() {
         const popCount = document.getElementById("popcount");
 
         const count = Number(popCount.innerText);
 
         popCount.innerText = count + 1;
 
-        // const cps = count / ((new Date().getTime() - startTime) / 1000);
-        updateCps()
-        console.log(cps);
+    
         
 
+        catImg.style.transform = 'scale(1.3)';
         catImg.src = "./static/img/catop.png";
 
         popAudio.play();
+
+        await updateCps()
     });
 
     clickArea.addEventListener("mouseup", function() {
+        catImg.style.transform = 'scale(1.0)';
         catImg.src = './static/img/cat.png';
     });
 
-    function updateCps() {
+    async function updateCps() {
+        cpsList.push(new Date().getTime());
         cps += 1;
-        setTimeout(() => {
-            cps -= 1;
-        }, 1000);
+
+        await wait(1000)
+        cps -= 1;
+
+        cpsListTemp = cpsList
+        cpsList = cpsList.filter((time) => {
+            return time > cpsList.at(-1) - 1000;
+        });
+
+        cpsList.unshift(cpsListTemp.at(-cpsList.length -1));
+
+        const cpsDifference = (cpsList.at(-1) - cpsList.at(0)) / 1000;
+
+        // console.log(cpsList, cpsListTemp);
+        // console.log(cpsDifference);
+        const calculatedCps = cpsList.length / Math.max(cpsDifference, 1);
+        currentCps = parseInt(calculatedCps * 100) / 100;
+
+        console.log(currentCps);
     }
 
 }
