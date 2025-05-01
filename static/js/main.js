af78a0ff2bed7d666d410735e1c05cbde2229696
fngk0921 <25s102@sunrint.hs.kr>
2025-05-01 19:34:43 +0900

    Enhance click functionality and update styles; add audio feedback and image change on click

--- static/js/main.js
+++ static/js/main.js
@@ -1,7 +1,38 @@
+window.onload = () => {
+    const clickArea = document.getElementById("app");
+    const catImg = document.getElementById("cat-img");
+    let cps = 0;
+    const popAudio = new Audio("https://popcat.click/pops/pop4.mp3");
+    popAudio.load();
 
+    const startTime = new Date().getTime();
 
-let cat_img = document.getElementById("cat-img");
-let pop_count = document.getElementById("pop-count");
-cat_img.addEventListener("click", function() {
+    clickArea.addEventListener("mousedown", function() {
+        const popCount = document.getElementById("popcount");
 
-})
+        const count = Number(popCount.innerText);
+
+        popCount.innerText = count + 1;
+
+        // const cps = count / ((new Date().getTime() - startTime) / 1000);
+        updateCps()
+        console.log(cps);
+        
+
+        catImg.src = "./static/img/catop.png";
+
+        popAudio.play();
+    });
+
+    clickArea.addEventListener("mouseup", function() {
+        catImg.src = './static/img/cat.png';
+    });
+
+    function updateCps() {
+        cps += 1;
+        setTimeout(() => {
+            cps -= 1;
+        }, 1000);
+    }
+
+}
